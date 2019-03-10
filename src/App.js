2f16b284306ba8951fecb19ae1efed0043667ca3
Rafael Cavalcante <rafael.ifrn@gmail.com>
2019-03-10 21:42:17 +0000

    my app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,23 +7,84 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <Container/>
+        <Footer/>
       </div>
     );
   }
 }
+
+
+class Header extends React.Component {
+  render() {
+    return ( 
+      <div className="header">
+       <h1>Header, I am a class header</h1>
+      </div>
+        )
+      }
+    }
+class Container extends React.Component {
+  render() {
+    return ( 
+      <div className="container">
+       <h1>Container, I am a class Container</h1>
+       <NutritionLabels/>
+      </div>
+        )
+      }
+    }
+class Footer extends React.Component {
+  render() {
+    return ( 
+      <div className="footer">
+       <h6>Footer, I am a class Footer</h6>
+      </div>
+        )
+      }
+    }
+
+  class NutritionLabels extends React.Component {
+    render() {
+      return (
+        <div className="nutrition-labels">
+          <div className="row">
+            <NutritionLabelItem/>
+            <NutritionLabelItem/>
+            <NutritionLabelItem/>
+          </div>
+          <div className="row">
+            <NutritionLabelItem/>
+            <NutritionLabelItem/>
+            <NutritionLabelItem/>
+          </div>
+        </div>
+      )
+    }
+  }
+
+class NutritionLabelItem extends React.Component {
+  render() {
+    return ( 
+      <div className="col">
+          <img src=""/>
+      </div>
+        )
+      }
+    }
+
+
+
+ReactDOM.render(
+  <div className="container">
+    <Header /> 
+    <Container />
+    <Footer />
+  </div>,
+  document.getElementById('root')
+);
+
+
 
 export default App;
